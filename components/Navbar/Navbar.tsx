02b841e221c4f787d97274374cac11c0fb1c4180
Aarya-S <aaryashelar000@gmail.com>
2023-02-20 09:50:56 +0530

    rohit at it again

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,10 +2,9 @@ import styles from './Navbar.module.css';
 import Link from 'next/link';
 import { getSession, removeSession } from '../../utils/sessionhandling';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
 const Navbar = () => {
-  const[session,setSession] = useState(getSession('user'));
+  const session = getSession('user');
   const navigate = useRouter();
   const signout = () => {
     removeSession('user');
@@ -14,14 +13,14 @@ const Navbar = () => {
   return (
     <>
     <div className={styles.navbar}>
-         <div className={styles.navbar__logo}>
+        <div className={styles.navbar__logo}>
             <Link className={styles.logoText} href="/">
             DigiZip 
             </Link>
         </div>
         <div className={styles.navbar__links}>
             <Link className={styles.navLink}  href="/about">About</Link>
-             {session==null?
+            {session==null?
               <Link className={styles.navLink}  href="/register">Register</Link>
             :
             <>
