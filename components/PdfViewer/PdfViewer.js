44cc4dbdd2758d764727736a42451a6248dff1de
RoHiT-engi <79554502+RoHiT-engi@users.noreply.github.com>
2023-03-06 19:49:34 +0530

    Added PDF Viewer

---
+++ components/PdfViewer/PdfViewer.js
@@ -0,0 +1,111 @@
+import * as PDFJS from 'pdfjs-dist';
+import { useRef, useState,useEffect } from 'react';
+PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.min.js`;
+
+const PdfViewer = (filedata) => {
+
+    const [file, setFiles] = useState(filedata);
+    let renderedPdf;
+    const pageRenderRef = useRef(null);
+    const DEFAULT_SCALE = 1;
+    let scale = 1;
+    // function zoomIn() {
+        
+    //     scale += 0.1;
+    //     draw();
+    // }
+    // function draw() {
+    //     // Clear the canvas
+    //     const canvas = document.getElementById('canvas-elem');
+    //     const ctx = canvas.getContext('2d');
+
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+    //     // Set the scale transform
+    //     ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        
+    //     // Draw on the canvas
+    //     // ...
+    //   }
+
+    useEffect(() => {
+        const loadingTask = PDFJS.getDocument({data : file.filedata});
+        loadingTask.promise.then(
+            (pdf) => {
+              renderedPdf = pdf;
+              const container = pageRenderRef.current;
+              const pageNo = 0;
+              fetchPageNo(pageNo, renderedPdf, container);
+            },
+            (error) => {
+              console.log("PDF error");
+            })
+      });
+    const fetchPageNo = (pageNo, pdf, container) => {
+        if (pageNo < pdf._pdfInfo.numPages) {
+          pageNo += 1;
+          createContextForCanvas(pdf, container, pageNo).then((page) => {
+            fetchPageNo(page + 1, pdf, container);
+          });
+        }
+      };
+
+    const createContextForCanvas = (pdf, container, pageNo) => {
+        return new Promise((resolve) => {
+          pdf.getPage(pageNo).then(async (page) => {
+            const viewport = page.getViewport({ scale: DEFAULT_SCALE });
+    
+            let canvasInHTML = {
+              canvas: undefined,
+              ctx: undefined
+            };
+    
+            const li = document.createElement("div");
+            li.setAttribute("id", "page-" + (page._pageIndex + 1));
+            li.setAttribute("style", "position: relative;");
+    
+            canvasInHTML.canvas = document.createElement("canvas");
+            canvasInHTML.ctx = canvasInHTML.canvas.getContext("2d");
+            canvasInHTML.canvas.height = viewport.height;
+            canvasInHTML.canvas.width = viewport.width;
+            canvasInHTML.canvas.imageSmoothingEnabled = false;
+            canvasInHTML.canvas.addEventListener('contextmenu', event => {
+                event.preventDefault();
+              });
+    
+            li.appendChild(canvasInHTML.canvas);
+            container.appendChild(li);
+    
+            const renderContext = {
+              canvasContext: canvasInHTML.ctx,
+              viewport
+            };
+    
+            let renderTask = page.render(renderContext);
+    
+            renderTask.promise.then(() => {
+              if (pageNo !== null) {
+                resolve(pageNo);
+              }
+            });
+    
+            if (pageRenderRef.current) {
+              pageRenderRef.current = li;
+            }
+          });
+        });
+      };
+
+    
+
+    return (
+        <div>
+            {/* <h1>PDF.js Sample Example</h1> */}
+            {/* <button onClick={zoomIn}>Zoom In</button>
+             */}
+            <div ref={pageRenderRef}></div>
+        </div>
+    )
+}
+
+export default PdfViewer;
--- pages/uploadfile.tsx
+++ pages/uploadfile.tsx
@@ -4,11 +4,18 @@ import {compressArrayBuffer, decompressArrayBuffer} from "../utils/CompressFile"
 import { encryptArrayBuffer,decryptArrayBuffer } from '../utils/EncryptFile';
 import {storeFiles} from '../utils/Web3Config&Functions';
 import { handleDownload } from '../utils/HandleDownload';
+import PdfViewer from '../components/PdfViewer/PdfViewer';
+
 export default function uploadfile() {
 
     const[file,setFile] = useState(new ArrayBuffer(0)); 
     const[outFile,setOutFile] = useState(new String);
     const[metadata,setMetadata] = useState(Object);
+    const[password,setPassword] = useState("");
+    const[orgcode,setOrgCode] = useState("");
+    const[date,setDate] = useState("");
+    const[downloadAccess,setDownloadAccess] = useState("");
+    const[readAccess,setReadAccess] = useState(false);
 
 
     const[err,setErr] = useState("");
@@ -29,24 +36,31 @@ export default function uploadfile() {
         }
     }
     const upload = async () => {
-        if(file!=null && file.byteLength>0){
-          const compressedFile = compressArrayBuffer(file);
-          setFile(compressedFile);
-          try{
-          const encryptedFile = encryptArrayBuffer(compressedFile, "sussysus");
-          setOutFile(encryptedFile);
-          // const CID = storeFiles(encryptedFile,"somename")
-          // console.log(CID);
-
-
-          }
-          catch(err){
-            console.log(err);
-          }
-          // console.log(decompressedFile);
+        console.log(downloadAccess);
+        if(password==""){
+          setErr(err +"\n Please enter a password");
+        }
+        if(orgcode==""){
+          setErr(err +"\nPlease enter a organization code");
+        }
+        if(file!=null && file.byteLength>0 && password!="" && orgcode!=""){
+          // const compressedFile = compressArrayBuffer(file);
+          // setFile(compressedFile);
+          // try{
+          // const encryptedFile = encryptArrayBuffer(compressedFile, "sussysus");
+          // setOutFile(encryptedFile);
           
+          // // console.log(decompressedFile);
+          // }catch(err){
+          //   console.log(err);
+          // }
+          console.log("Uploading file");
         // console.log(compressedFile);
         }
+        
+    }
+
+    const download = async () => {
         
     }
 
@@ -54,14 +68,42 @@ export default function uploadfile() {
 
     return (
         <>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div>
+            <input type="file" onChange={(e)=>handleChange(e)} accept="application/pdf"></input>
+            <button onClick={upload} hidden={file.byteLength==0}>Upload</button>
+            <br></br>
+            <br></br>
+            <input 
+              type="password" 
+              placeholder="Enter password for File Encryption" 
+              value={password}
+              onChange={(e)=>{setPassword(e.target.value)}} ></input>
 
-        <input type="file" onChange={(e)=>handleChange(e)} accept="application/pdf"></input>
-        <button onClick={upload} hidden={file.byteLength==0}>Upload</button>
-        <br></br>
+            {password==""?<button onClick={()=>setPassword("somethingdefault")}>Select Default</button>:""}
+            <br></br>
+            Enter Alphanumeric code of Organization to which you want to share the file with
+            <input type="text" placeholder="Enter code" onChange={(e)=>setOrgCode(e.target.value)}></input>
+            <br></br>
+            Select the Access Date 
+            <input type="date" placeholder="Enter code" onChange={(e)=>setDate(e.target.value)}></input>
+            <br></br>
+          
+            {/* yaha pe radio input vagara daalde based on value wo downloadAccess & readAccess ka state Set or non Set karde */}
+           
+          </div>
+
+          <div>
+            YAHA CUSTOM PRESETS DAALDE AARYA
+          </div>
+
+          <div>
+            YAHA STATIC PRESETS DAALDE AARYA
+          </div>
+          <br></br>
         {err}
         </>
     )
