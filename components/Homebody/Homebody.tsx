1d86da5aecf717a40d16a7b785d93dc61fdfc827
Pranav <pranavyede@gmail.com>
2023-03-12 21:08:38 +0530

    some minute imp changes

--- components/Homebody/Homebody.tsx
+++ components/Homebody/Homebody.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload, faShareAlt, faEye } from "@fortawesome/free-solid-svg-icons";
+import { faUpload, faShareAlt, faEye, faDownload, faFileCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import styles from './Homebody.module.css';
 import { getSession } from "../../utils/sessionhandling";
 import React, {useState,useEffect} from 'react';
@@ -12,19 +12,20 @@ const Homebody = () => {
     const Userlinks = () => {
       return(
         <>
-        <h1>Welcome {session.email.split('@')[0]}</h1>
+        <div className={styles.welcome}>Hey {session.email.split('@')[0]}, welcome to DigiZip!</div>
         <div className={styles.home__buttons}>
           <Link className={styles.view__files} href="/viewfiles">
             <FontAwesomeIcon icon={faEye} style={{marginRight: 20}}/> View Files
           </Link>
 
-          <Link className={styles.share__file} href="/sharefile">
-            <FontAwesomeIcon icon={faShareAlt} style={{marginRight: 20}} /> Share File
+          <Link className={styles.add__file} href="/addfile">
+            <FontAwesomeIcon icon={faFileCirclePlus} style={{marginRight: 20}} /> Add Files
           </Link>
 
-          <Link className={styles.add__file} href="/uploadfile">
-            <FontAwesomeIcon icon={faUpload} style={{marginRight: 20}} /> Upload Files
+          <Link className={styles.share__file} href="/sharefile">
+            <FontAwesomeIcon icon={faUpload} style={{marginRight: 20}} /> Share Files
           </Link>
+
         </div></>
       )
     }
@@ -32,14 +33,14 @@ const Homebody = () => {
     const Orglinks = () => {
       return(
         <>
-        <h1>Wwelcome {orgdash.name}</h1>
+        <div className={styles.welcome}>Welcome {orgdash.name}!</div>
       <div className={styles.home__buttons}>
           <Link className={styles.view__files} href="/orgviewfile">
             <FontAwesomeIcon icon={faEye} style={{marginRight: 20}}/> View Files
           </Link>
 
           <Link className={styles.share__file} href="/orgreqfile">
-            <FontAwesomeIcon icon={faShareAlt} style={{marginRight: 20}} /> Request File
+            <FontAwesomeIcon icon={faDownload} style={{marginRight: 20}} /> Request Files 
           </Link>
       </div>
       </>)
