4451368b1aed80ff57c3b698bbc974ab023b09a3
Pranav <pranavyede@gmail.com>
2023-03-24 16:52:10 +0530

    org req file done

--- pages/orgreqfile.tsx
+++ pages/orgreqfile.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import OrgViewCard from "../components/OrgComponent/OrgViewcard";
 import { createsession, getSession } from "../utils/sessionhandling";
-import styles from "../styles/UserHome.module.css";
+import styles from "../styles/Orgreqfile.module.css";
 export default function orgreqfile(){
     const orgdetail = getSession('orgdetail') || {};
     // const client = axios.create({
@@ -10,20 +10,24 @@ export default function orgreqfile(){
     //     baseURL: "http://localhost:5000"
     // })
     return (
-        <div className={styles.UserHomeBody}>
-            <br />
-            <br />
-            <br />
-            <label color="white">User Email : - </label><br/>
-            <input type="email" placeholder="Enter the email of the user you want to request" /><br/>
-            <label color="white">Title of Request </label><br/>
-            <input type="text" placeholder="Enter the hash of the file you want to request" /><br/>
-            <label>Default text : - </label><br/>
-            orgcode : {orgdetail.generated_hash}<br/>
-            orgname : {orgdetail.name}<br/>
+        <div className={styles.window1}>
+            <div className={styles.card}>
+                <div className = {styles.pp}>
+            <h2 style={{textAlign:'center',letterSpacing:'0.5px'}}>Request Files from User <hr style={{color:'white'}}/></h2><br />
+            <label color="white">Username/Email ID :- </label><br/>
+            <input type="email" style={{width:'25vw'}} placeholder="Enter the Username/Email ID of the user" /><br/><br />
+            <label color="white">Purpose/Subject :- </label><br/>
+            <input style={{width:'25vw'}} type="text" placeholder="Enter the title of request" /><br/><br />
             <label>Enter Description : - </label><br/>
-            <textarea placeholder=""/><br/>
-            <button>Request</button>
+            <textarea cols={60} rows={4} maxLength={1000} placeholder="Specify the required documents and other details (optional)"/><br/><br /><hr />
+            <label style={{color:'gray', fontStyle:'italic'}}>User will be able to view your following details while receiving request :- </label><br/>
+            <label style={{paddingTop:'10px'}} htmlFor="admin">Organization Name -</label>
+            <input style={{marginLeft:'15px', borderRadius:'10px'}} value={orgdetail.name} readOnly/> <br />
+            <label style={{paddingTop:'10px'}} htmlFor="admin">Organization Code -</label>
+            <input style={{marginLeft:'18px', borderRadius:'10px'}} value={orgdetail.generated_hash} readOnly/> <br /><br />
+           <button className={styles.pov}>Request</button>
+        </div>
+        </div>
         </div>
     )
 }
